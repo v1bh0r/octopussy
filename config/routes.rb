56fdfd5f82e8b060b17556a41b6fec5a96780ad2
suresh <suresh.kumar@trantorinc.com>
2012-10-13 17:30:12 +0530

    github details thro github_api

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Octopussy::Application.routes.draw do
 
   # Sample of regular route:
      match 'milestone' => 'project#milestone'
-    # match 'milestone' => 'project#collaboraters'
+     match 'milestone' => 'project#collaboraters'
   # Keep in mind you can assign values other than :controller and :action
 
   # Sample of named route:
