2e66942b0b2fdae4aad8040c66a68c317eac3e4d
Vibhor Mahajan <vibhor.mahajan@gmail.com>
2012-10-13 13:06:30 +0530

    Misc ui + added project controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  before_filter :when_logged_in
+
   def index
   end
 
@@ -9,5 +11,10 @@ class HomeController < ApplicationController
   end
 
   def contact
+  end
+
+  private
+  def when_logged_in
+    redirect_to project_index_path if user_signed_in?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Octopussy::Application.routes.draw do
+  get "project/index"
+
+  get "project/show"
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   get "home/index"
