3f86c8738ae2cf7a04e284e5b6680a85b2fb1b42
Vibhor Mahajan <vibhor.mahajan@gmail.com>
2012-10-13 08:07:24 +0530

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Octopussy::Application.config.secret_token = 'e31468e9c22b223a0f83dcd9fb509b645b2f50a4d55daf7da6f16d0fcd25d754f4210409e3c10d2e8af8c64fea57dbaa63ddf79126525e2404b13601cd63a4cb'
