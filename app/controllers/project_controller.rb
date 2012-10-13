7d06b86e94314c7109771561777237217e7be418
suresh <suresh.kumar@trantorinc.com>
2012-10-13 18:39:00 +0530

    milestone list

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -2,9 +2,12 @@ class ProjectController < ApplicationController
   def index
     github = Github.new(oauth_token: current_user.oauth_token)
     @repos = github.repos.all
-    @issues = github.issues
-    @milestones = github.issues.milestones 
-  end
+  
+    @issues = github.issues.all 
+    
+    @milestones = github.issues.milestones.all
+    raise @milestones.inspect
+ end
 
   def show
   end
