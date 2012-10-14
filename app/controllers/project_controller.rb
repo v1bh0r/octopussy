f15cb3614c7491755fd3250339f14fa46858acc4
suresh <suresh.kumar@trantorinc.com>
2012-10-14 10:31:03 +0530

    latest code pull

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -1,11 +1,5 @@
 class ProjectController < ApplicationController
   def index
-    github = Github.new(oauth_token: current_user.oauth_token)
-    @repos = github.repos.all
-  
-    @issues = github.issues.all 
-    
-    @milestones = github.issues.milestones
    
  end
 
